4c11aa378160cf8da78001712272b7d8b2c32384
SCBee <77908243+SCBee@users.noreply.github.com>
2021-03-22 18:27:13 -0600

    Filler

--- main.cpp
+++ main.cpp
@@ -16,28 +16,28 @@ int main(int, char**)
     std::vector<indicator::Data> dataset;
 
     // Import our downloaded data
-    dataset = indicator::importData("DATA/AACG.csv");
+    // dataset = indicator::importData("DATA/AACG.csv");
 
     // Calculate SMA (14 day period)
-    indicator::MA ma;
-    ma.simpleMA(dataset, 14);
+    // indicator::MA ma;
+    // ma.simpleMA(dataset, 14);
 
-    for (auto i : dataset)
-    {
-        std::cout << i;
-    }
+    // for (auto i : dataset)
+    // {
+    //     std::cout << i;
+    // }
 
     // Defaults to Daily but there are a options like weekly and monthly
-    //yahoo.setInterval(DAILY);
+    // yahoo.setInterval(DAILY);
 
     // Download the data & save into csv file
-    //yahoo.getData({"AAPL"}, "2020-01-01", "2021-01-01");
+    // yahoo.getData({"AAPL"}, "2020-01-01", "2021-01-01");
 
     // Download and create a vector of tickers to download from Yahoo Finance
-    //ticker.getNASDAQTickers();
+    // ticker.getNASDAQTickers();
 
     // Get the sweet, sweet honey from Yahoo Finance!
-    //yahoo.getData(ticker.getTickerList(), "2020-01-01", "2021-01-01");
+    // yahoo.getData(ticker.getTickerList(), "2020-01-01", "2021-01-01");
 
     return 0;
 }
